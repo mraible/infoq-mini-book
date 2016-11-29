9e426497e389d68a819e4e6c474933cd41ec1611
Matt Raible <matt@raibledesigns.com>
2016-11-29 00:19:33 -0700

    Updates to match JHipster Book 2.0

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'coderay'
+gem 'asciidoctor-pdf', github: 'asciidoctor/asciidoctor-pdf', ref: 'cffe2d41a3055a8cfc55e37b12513f69a7e1484a'
---
+++ generate-pdf.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Usage: `./generate-pdf.sh` to generate printable 6x9" PDF
+#        `./generate-pdf.sh screen` to generate a downloadable 8.5x11" PDF
+
+source $HOME/.rvm/scripts/rvm
+
+rvm use 2.3.1 --quiet
+if [ ! -d .bundle/gems ]; then
+  rm -f Gemfile.lock
+  bundle config --local github.https true
+  bundle --path=.bundle/gems --binstubs=.bundle/.bin
+fi
+
+ASCIIDOCTOR_PDF="./.bundle/.bin/asciidoctor-pdf"
+OPTIMIZE_PDF="`bundle exec gem contents --show-install-dir asciidoctor-pdf`/bin/optimize-pdf"
+
+ROOT_DIR=$(realpath $(dirname $0))
+MEDIA=prepress
+if [ ! -z "$1" ]; then
+  MEDIA=$1
+fi
+BASE_DIR="$ROOT_DIR/src/docs/asciidoc"
+OUT_DIR="$ROOT_DIR/build/asciidoc/pdf-$MEDIA"
+
+$ASCIIDOCTOR_PDF --trace -B "$BASE_DIR" \
+  -D "$OUT_DIR" \
+  -S unsafe \
+  -r "$ROOT_DIR/src/main/ruby/asciidoctor-pdf-extensions.rb" \
+  -a media=$MEDIA \
+  -a pdfmarks \
+  -a pdf-style=infoq-$MEDIA \
+  -a pdf-stylesdir="$BASE_DIR/styles/pdf" \
+  -a pdf-fontsdir="$BASE_DIR/styles/pdf/fonts" \
+  -a sourcedir=../../../main/webapp \
+  -a source-highlighter=coderay \
+  -a imagesdir=images \
+  -a toc \
+  -a icons=font \
+  -a idprefix \
+  -a idseparator=- \
+  -a projectdir=../../.. \
+  -a rootdir=../../.. \
+  -a project-name=jhipster-book \
+  -a project-version=2.0.0-SNAPSHOT \
+  -a attribute-missing=warn \
+  "$BASE_DIR/index.adoc"
+
+$OPTIMIZE_PDF "$OUT_DIR/index.pdf"
+mv -f "$OUT_DIR/index-optimized.pdf" "$OUT_DIR/index.pdf"
